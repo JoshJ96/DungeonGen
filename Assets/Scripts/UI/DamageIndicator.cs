0a7c9be1d6a9e1a21ab7d5c7ab5c22178aba218f
Joshua Jaworowski <joshuajaworowski@gmail.com>
2021-02-05 11:34:56 -0500

    Damage Popups

---
+++ Assets/Scripts/UI/DamageIndicator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageIndicator : MonoBehaviour
+{
+    Animator animator;
+    public TextMeshPro text;
+    void Start()
+    {
+        GameEvents.instance.doDamage += DoDamage;
+        animator = GetComponent<Animator>();
+        text = GetComponent<TextMeshPro>();
+    }
+
+    private void DoDamage(Unit attacking, Unit takingDamage, int damage)
+    {
+        text.text = $"{damage}";
+        animator.SetTrigger("DamagePopUp");
+    }
+
+    void HidePopup()
+    {
+        animator.SetTrigger("DamagePopUpDisable");
+    }
+}
