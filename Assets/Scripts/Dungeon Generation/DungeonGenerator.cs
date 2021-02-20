b5b49475996c5f5e7847d4c398f3e0869eafa9f2
Joshua Jaworowski <joshuajaworowski@gmail.com>
2021-02-20 11:03:05 -0500

    Room generation

--- Assets/Scripts/Dungeon Generation/DungeonGenerator.cs
+++ Assets/Scripts/Dungeon Generation/DungeonGenerator.cs
@@ -12,6 +12,11 @@ public class Divider
     public int x1, x2, y1, y2;
 }
 
+public class Room
+{
+    public int x1, x2, y1, y2;
+}
+
 public class DungeonGenerator : MonoBehaviour
 {
     enum Axis
@@ -35,9 +40,10 @@ public class DungeonGenerator : MonoBehaviour
     //BSP Divider Root
     Divider root;
     List<Divider> dividerList = new List<Divider>();
+    List<Room> roomList = new List<Room>();
 
     //The % of random coords from middle to be chosen (0.05 = 5%)
-    [Range(0, 1)]
+    [Range(0, 0.1f)]
     public float bspDeviation = 0.05f;
 
     private void Start()
@@ -48,7 +54,31 @@ public class DungeonGenerator : MonoBehaviour
     private void GenerateDungeon()
     {
         Initialize_BSP_Root();
-        BSP_Split(0);
+        Recursive_BSP_Split(0);
+        Random_Place_Rooms();
+    }
+
+    private void Random_Place_Rooms()
+    {
+        foreach (var divider in dividerList)
+        {
+            //Generate a random room
+            int width = UnityEngine.Random.Range(roomMinSize, roomMaxSize);
+            int height = UnityEngine.Random.Range(roomMinSize, roomMaxSize);
+
+            int _x1 = UnityEngine.Random.Range(divider.x1, divider.x2 - width);
+            int _y1 = UnityEngine.Random.Range(divider.y1, divider.y2 - height);
+            int _x2 = _x1 + width;
+            int _y2 = _y1 + height;
+            Room room = new Room
+            {
+                x1 = _x1,
+                x2 = _x2,
+                y1 = _y1,
+                y2 = _y2
+            };
+            roomList.Add(room);
+        }
     }
 
     private void Initialize_BSP_Root()
@@ -66,9 +96,9 @@ public class DungeonGenerator : MonoBehaviour
         dividerList.Add(root);
     }
 
-    int deleteThis = 400;
+    int deleteThis = 500;
 
-    private void BSP_Split(int currentDepth)
+    private void Recursive_BSP_Split(int currentDepth)
     {
         deleteThis--;
         if (deleteThis <= 0)
@@ -81,7 +111,7 @@ public class DungeonGenerator : MonoBehaviour
 
         if (toCarve.Count != 0)
         {
-            foreach (var item in dividerList.Where(x => x.depth == currentDepth).ToList())
+            foreach (var item in toCarve)
             {
                 //Pick a random axis
                 int rng = UnityEngine.Random.Range(0, 2);
@@ -101,7 +131,7 @@ public class DungeonGenerator : MonoBehaviour
                 }
 
                 //Y
-                if (rng == 1)
+                else if (rng == 1)
                 {
                     //If it can't be carved along the Y axis, switch and try X axis
                     if (DividerCanBeCarvedY(item))
@@ -119,10 +149,9 @@ public class DungeonGenerator : MonoBehaviour
         {
             return;
         }
-        print(currentDepth);
-        BSP_Split(currentDepth + 1);
+        print("depth: " + currentDepth + ", num divs: " + dividerList.Count);
+        Recursive_BSP_Split(currentDepth + 1);
     }
-
 
     private void CarveDivider(Divider divider, Axis axis)
     {
@@ -155,6 +184,7 @@ public class DungeonGenerator : MonoBehaviour
             dividerList.Add(left);
             divider.leftChild = left;
             divider.rightChild = right;
+            dividerList.Remove(divider);
         }
         else if (axis == Axis.Y)
         {
@@ -185,37 +215,53 @@ public class DungeonGenerator : MonoBehaviour
             dividerList.Add(left);
             divider.leftChild = left;
             divider.rightChild = right;
+            dividerList.Remove(divider);
         }
     }
 
     private bool DividerCanBeCarvedX(Divider divider)
     {
-        return !((divider.x2 - divider.x1) < (roomMaxSize * 2));
+        return !((divider.x2 - divider.x1) < (roomMaxSize * 2.5));
     }
 
     private bool DividerCanBeCarvedY(Divider divider)
     {
-        return !((divider.y2 - divider.y1) < (roomMaxSize * 2));
+        return !((divider.y2 - divider.y1) < (roomMaxSize * 2.5));
     }
 
     private void OnDrawGizmos()
     {
+        foreach (var room in roomList)
+        {
+            for (int x = room.x1; x < room.x2; x++)
+            {
+                for (int y = room.y1; y < room.y2; y++)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawCube(new Vector3(x, 0, y), Vector3.one);
+                }
+            }
+        }
+
 
         foreach (var item in dividerList)
         {
-            for (int x = item.x1; x < item.x2 - 1; x++)
             {
-                for (int y = item.y1; y < item.y2 - 1; y++)
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(new Vector3((item.x2+item.x1)/2, 0, (item.y2+item.y1)/2), Vector3.one);
+
+                for (int x = item.x1; x < item.x2 - 1; x++)
                 {
-                    if (x == item.x1 || x == item.x2 - 2 || y == item.y1 || y == item.y2 - 2)
+                    for (int y = item.y1; y < item.y2 - 1; y++)
                     {
-                        Gizmos.DrawCube(new Vector3(x, 0, y), Vector3.one);
+                        if (x == item.x1 || x == item.x2 - 2 || y == item.y1 || y == item.y2 - 2)
+                        {
+                            Gizmos.color = Color.white;
+                            Gizmos.DrawCube(new Vector3(x, 0, y), Vector3.one);
+                        }
                     }
                 }
             }
         }
     }
 }
-
-
-
