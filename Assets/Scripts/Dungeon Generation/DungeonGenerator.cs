46983a637162a4bb4e8917ed13c8df95632c5260
Joshua Jaworowski <Joshua.Jaworowski@trustwave.com>
2021-02-23 11:32:29 -0600

    Code cleanup so far

--- Assets/Scripts/Dungeon Generation/DungeonGenerator.cs
+++ Assets/Scripts/Dungeon Generation/DungeonGenerator.cs
@@ -46,8 +46,8 @@ public class Hallway
 
 public class DungeonGenerator : MonoBehaviour
 {
-    enum Axis
-    {
+    //Axis enum
+    enum Axis {
         X,
         Y
     }
@@ -64,16 +64,24 @@ public class DungeonGenerator : MonoBehaviour
     [Range(5, 100)]
     public int roomMaxSize;
 
+    //Map
     public Node[,] map;
 
     //BSP Divider Root
     Divider root;
+
+    //Lists of dungeon components
     List<Divider> dividerList = new List<Divider>();
     List<Room> roomList = new List<Room>();
     List<Hallway> hallwayList = new List<Hallway>();
+
+    //Used to mark the final division of the dungeons
     int finalIteration = 0;
 
-    //Map objects
+    //Number of room divisions to try
+    public int numberDivides = 6;
+
+    //Map generate objects
     public GameObject floor;
     public GameObject wall;
 
@@ -90,6 +98,7 @@ public class DungeonGenerator : MonoBehaviour
 
     private void GenerateDungeon()
     {
+        Initialize_Node_Map();
         Initialize_BSP_Root();
         Recursive_BSP_Split(0);
         Random_Place_Rooms();
@@ -97,13 +106,14 @@ public class DungeonGenerator : MonoBehaviour
         Instantiate_Terrain();
     }
 
-    private void Instantiate_Terrain()
+    private void Initialize_Node_Map()
     {
         map = new Node[mapWidth, mapHeight];
 
-        for (int x = 0; x < mapWidth; x++)
+        //Default wall type
+        for (int x = 0; x < mapWidth - 1; x++)
         {
-            for (int y = 0; y < mapHeight; y++)
+            for (int y = 0; y < mapHeight - 1; y++)
             {
                 map[x, y] = new Node
                 {
@@ -114,24 +124,153 @@ public class DungeonGenerator : MonoBehaviour
                 };
             }
         }
+    }
 
+    private void Initialize_BSP_Root()
+        {
+            //Create the initial root divider
+            root = new Divider
+            {
+                id = 0,
+                depth = 0,
+                x1 = 0,
+                y1 = 0,
+                x2 = mapWidth,
+                y2 = mapHeight
+            };
+
+            dividerList.Add(root);
+        }
+
+    private void Recursive_BSP_Split(int currentDepth)
+        {
+            numberDivides--;
+            if (numberDivides <= 0)
+            {
+                return;
+            }
+
+            //Loop through every divider at the current depth
+            List<Divider> toCarve = dividerList.Where(x => x.depth == currentDepth).ToList();
+
+            if (toCarve.Count != 0)
+            {
+                int count = 0;
+                foreach (var item in toCarve)
+                {
+                    //Pick a random axis
+                    int rng = UnityEngine.Random.Range(0, 2);
+
+                    //X
+                    if (rng == 0)
+                    {
+                        //If it can't be carved along the X axis, switch and try Y axis
+                        if (DividerCanBeCarved(item, Axis.X))
+                        {
+                            CarveDivider(item, Axis.X);
+                        }
+                        else if (DividerCanBeCarved(item, Axis.Y))
+                        {
+                            CarveDivider(item, Axis.Y);
+                        }
+                    }
+
+                    //Y
+                    else if (rng == 1)
+                    {
+                        //If it can't be carved along the Y axis, switch and try X axis
+                        if (DividerCanBeCarved(item, Axis.Y))
+                        {
+                            CarveDivider(item, Axis.Y);
+                        }
+                        else if (DividerCanBeCarved(item, Axis.X))
+                        {
+                            CarveDivider(item, Axis.X);
+                        }
+                    }
+                    count++;
+                }
+            }
+            else
+            {
+                return;
+            }
+            finalIteration++;
+            Recursive_BSP_Split(currentDepth + 1);
+        }
+
+    private void Random_Place_Rooms()
+        {
+            foreach (var divider in dividerList.Where(x => x.depth == finalIteration))
+            {
+                //Generate a random room
+                int width = UnityEngine.Random.Range(roomMinSize, roomMaxSize - 1);
+                int height = UnityEngine.Random.Range(roomMinSize, roomMaxSize - 1);
+
+                int _x1 = UnityEngine.Random.Range(divider.x1, divider.x2 - width);
+                int _y1 = UnityEngine.Random.Range(divider.y1, divider.y2 - height);
+                int _x2 = _x1 + width;
+                int _y2 = _y1 + height;
+
+                Room room = new Room
+                {
+                    x1 = _x1,
+                    x2 = _x2,
+                    y1 = _y1,
+                    y2 = _y2
+                };
+
+                divider.roomWithin = room;
+                roomList.Add(room);
+            }
+        }
+
+    private void Generate_Hallways()
+        {
+
+            //Get dividers at finalIteration - 1 (to access child nodes)
+            foreach (var divider in dividerList.Where(x => x.depth == finalIteration-1))
+            {
+                if (divider.leftChild != null && divider.rightChild != null)
+                {
+                    Create_Hallway(divider.leftChild.roomWithin, divider.rightChild.roomWithin);
+                }
+            }
+
+            int count = 2;
+            while (count < finalIteration + 1)
+            {
+                //Get dividers at finalIteration - 2 (to access child nodes)
+                foreach (var divider in dividerList.Where(x => x.depth == finalIteration - count))
+                {
+                    if (divider.leftChild != null && divider.rightChild != null)
+                    {
+                        var room2 = divider.leftChild.FindRoomsWithin(roomList);
+                        var room1 = divider.rightChild.FindRoomsWithin(roomList);
+
+                        if (room1 != null && room2 != null)
+                        {
+                            if (room1.Count != 0 && room2.Count != 0)
+                            {
+                                Create_Hallway(room1[0], room2[0]);
+                            }
+                        }
+                    }
+                }
+                count++;
+            }
+        }
+
+    private void Instantiate_Terrain()
+    {
         //Rooms
         foreach (var room in roomList)
         {
-            for (int x = room.x1; x < room.x2-1; x++)
+            for (int x = room.x1; x < room.x2; x++)
             {
-                for (int y = room.y1; y < room.y2-1; y++)
+                for (int y = room.y1; y < room.y2; y++)
                 {
-                    try
-                    {
-                        map[x, y].walkable = true;
-
-                    }
-                    catch (Exception)
-                    {
-
-                        throw;
-                    }
+                    map[x, y].walkable = true;
                 }
             }
         }
@@ -165,73 +304,36 @@ public class DungeonGenerator : MonoBehaviour
             }
         }
 
+
         grid.SetActive(true);
         grid.GetComponent<Grid>().gridSizeX = mapWidth;
         grid.GetComponent<Grid>().gridSizeY = mapHeight;
         grid.GetComponent<Grid>().grid = map;
+        FindObjectOfType<BoardManager>().worldGrid = grid.GetComponent<Grid>();
 
 
     }
 
-    private void Random_Place_Rooms()
+
+    /*
+    ██╗░░██╗███████╗██╗░░░░░██████╗░███████╗██████╗░░██████╗
+    ██║░░██║██╔════╝██║░░░░░██╔══██╗██╔════╝██╔══██╗██╔════╝
+    ███████║█████╗░░██║░░░░░██████╔╝█████╗░░██████╔╝╚█████╗░
+    ██╔══██║██╔══╝░░██║░░░░░██╔═══╝░██╔══╝░░██╔══██╗░╚═══██╗
+    ██║░░██║███████╗███████╗██║░░░░░███████╗██║░░██║██████╔╝
+    ╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═════╝░
+    */
+
+    private bool DividerCanBeCarved(Divider divider, Axis axis)
     {
-        foreach (var divider in dividerList.Where(x => x.depth == finalIteration))
+        switch (axis)
         {
-            //Generate a random room
-            int width = UnityEngine.Random.Range(roomMinSize, roomMaxSize);
-            int height = UnityEngine.Random.Range(roomMinSize, roomMaxSize);
-
-            int _x1 = UnityEngine.Random.Range(divider.x1, divider.x2 - width);
-            int _y1 = UnityEngine.Random.Range(divider.y1, divider.y2 - height);
-            int _x2 = _x1 + width;
-            int _y2 = _y1 + height;
-
-            Room room = new Room
-            {
-                x1 = _x1,
-                x2 = _x2,
-                y1 = _y1,
-                y2 = _y2
-            };
-
-            divider.roomWithin = room;
-            roomList.Add(room);
-        }
-    }
-
-    private void Generate_Hallways()
-    {
-
-        //Get dividers at finalIteration - 1 (to access child nodes)
-        foreach (var divider in dividerList.Where(x => x.depth == finalIteration-1))
-        {
-            if (divider.leftChild != null && divider.rightChild != null)
-            {
-                Create_Hallway(divider.leftChild.roomWithin, divider.rightChild.roomWithin);
-            }
-        }
-
-        int count = 2;
-        while (count < finalIteration + 1)
-        {
-            //Get dividers at finalIteration - 2 (to access child nodes)
-            foreach (var divider in dividerList.Where(x => x.depth == finalIteration - count))
-            {
-                if (divider.leftChild != null && divider.rightChild != null)
-                {
-                    var room2 = divider.leftChild.FindRoomsWithin(roomList);
-                    var room1 = divider.rightChild.FindRoomsWithin(roomList);
-
-                    if (room1 != null && room2 != null)
-                    {
-                        if (room1.Count != 0 && room2.Count != 0)
-                        {
-                            Create_Hallway(room1[0], room2[0]);
-                        }
-                    }
-                }
-            }
-            count++;
+            case Axis.X:
+                return !((divider.x2 - divider.x1) < (roomMaxSize * 2));
+            case Axis.Y:
+                return !((divider.y2 - divider.y1) < (roomMaxSize * 2));
+            default:
+                return false;
         }
     }
 
@@ -349,81 +451,6 @@ public class DungeonGenerator : MonoBehaviour
         }
     }
 
-    private void Initialize_BSP_Root()
-    {
-        //Create the initial root divider
-        root = new Divider
-        {
-            id = 0,
-            depth = 0,
-            x1 = 0,
-            y1 = 0,
-            x2 = mapWidth,
-            y2 = mapHeight
-        };
-
-        dividerList.Add(root);
-    }
-
-    public int numberDivides = 6;
-
-    private void Recursive_BSP_Split(int currentDepth)
-    {
-        numberDivides--;
-        if (numberDivides <= 0)
-        {
-            return;
-        }
-
-        //Loop through every divider at the current depth
-        List<Divider> toCarve = dividerList.Where(x => x.depth == currentDepth).ToList();
-
-        if (toCarve.Count != 0)
-        {
-            int count = 0;
-            foreach (var item in toCarve)
-            {
-                //Pick a random axis
-                int rng = UnityEngine.Random.Range(0, 2);
-
-                //X
-                if (rng == 0)
-                {
-                    //If it can't be carved along the X axis, switch and try Y axis
-                    if (DividerCanBeCarvedX(item))
-                    {
-                        CarveDivider(item, Axis.X);
-                    }
-                    else if (DividerCanBeCarvedY(item))
-                    {
-                        CarveDivider(item, Axis.Y);
-                    }
-                }
-
-                //Y
-                else if (rng == 1)
-                {
-                    //If it can't be carved along the Y axis, switch and try X axis
-                    if (DividerCanBeCarvedY(item))
-                    {
-                        CarveDivider(item, Axis.Y);
-                    }
-                    else if (DividerCanBeCarvedX(item))
-                    {
-                        CarveDivider(item, Axis.X);
-                    }
-                }
-                count++;
-            }
-        }
-        else
-        {
-            return;
-        }
-        finalIteration++;
-        Recursive_BSP_Split(currentDepth + 1);
-    }
-
     private void CarveDivider(Divider divider, Axis axis)
     {
         if (axis == Axis.X)
@@ -490,17 +517,5 @@ public class DungeonGenerator : MonoBehaviour
             divider.leftChild = left;
             divider.rightChild = right;
         }
-    }
-
-    public int layer;
-
-    private bool DividerCanBeCarvedX(Divider divider)
-    {
-        return !((divider.x2 - divider.x1) < (roomMaxSize * 2));
-    }
-
-    private bool DividerCanBeCarvedY(Divider divider)
-    {
-        return !((divider.y2 - divider.y1) < (roomMaxSize * 2));
     }
 }
